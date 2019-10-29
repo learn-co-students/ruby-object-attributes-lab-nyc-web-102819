d8a99dd3e2f38c29718b8e2011783676b0582b95
Aaron Klotnia <aklotnia@gmail.com>
2019-10-29 14:09:16 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def name=(name_human)
+        @name = name_human
+    end
+
+    def name
+        @name
+    end
+
+    def job=(human_job)
+        @job = human_job
+    end
+
+    def job
+        @job
+    end
+end
